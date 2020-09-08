9ebc4a14720756f9daf40728262616277c7a8396
vivek666 <amalhan43@gmail.com>
2020-09-09 02:56:23 +0530

    Header implemented

---
+++ InfoBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+function InfoBox({ title, cases, total }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        <Typography color="textSecondary">{title}</Typography>
+        <h2 className="infoBox__cases">{cases}</h2>
+        <Typography color="textSecondary" className="infoBox__total">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  //USE EFFECT Implemented for React hooks
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            names: country.country,
+            value: country.countryInfo.iso3,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}> {country.name} </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats"></div>
+
+      {/* Map + table */}
     </div>
   );
 }
